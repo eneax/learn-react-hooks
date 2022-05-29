ad0412bf9bbe0bed569b497450d3446d772249bc
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-05-29 12:32:25 +0200

    Custom Hooks - useWait exercise

---
+++ custom-hooks/01-useWait/starter.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+/*
+  Instructions:
+    Finish implementing the `useWait` custom Hook.
+    `useWait` should return a boolean that changes from
+    `false` to `true` after `delay` seconds. 
+*/
+
+function useWait(delay) {
+  return true;
+}
+
+function Wait({ delay = 1000, placeholder, ui }) {
+  const show = useWait(delay);
+
+  return show === true ? ui : placeholder;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Wait
+        delay={3000}
+        placeholder={<p>Waiting...</p>}
+        ui={<p>This text should appear after 3 seconds.</p>}
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
