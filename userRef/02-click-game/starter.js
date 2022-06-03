91e324e5c084df624cfad48f01a51dfd861cc356
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-06-03 14:11:32 +0200

    useRef - click game exercise

---
+++ userRef/02-click-game/starter.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+/*
+  Instructions:
+    You're building an app to see how many times you can click
+    a button in 10 seconds. 
+
+    The UI has three parts, a button, a timer counting down from 10,
+    and a count of how many times you've clicked the button.
+
+    Once the timer reaches 0, remove the button from the UI.
+*/
+
+function CounterGame() {
+  return <div className="App">See instructions.</div>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<CounterGame />, rootElement);
